2f22160c4ee6f15908c68939266d9a16ae78446a
Si-Hyeak-Kang <zlcls456@gmail.com>
2022-01-01 23:14:17 +0900

    나열된 수에서 최솟값과 최댓값 구하기(오류해결)

--- src/fastCampus/arg01/MinMaxProblem.java
+++ src/fastCampus/arg01/MinMaxProblem.java
@@ -4,24 +4,24 @@ public class MinMaxProblem {
 
     public static void main(String[] args) {
 
-        int[] numbers = {10, 55, 23, 2, 79, 101, 16, 82, 30, 45};
+        int[] numbers = {10, 55, 23, 2, 79, 101, 16, 182, 30, 45};
 
         int min = numbers[0]; // 초기값 설정 : 배열 numbers 안에서 0번째
         int max = numbers[0]; // 초기값 설정 : 배열 numbers 안에서 0번째
 
-        int minPos = 1; // 최솟값 위치 변수
-        int maxPos = 1; // 최댓값 위치 변수
+        int minPos = 0; // 최솟값 위치 변수
+        int maxPos = 0; // 최댓값 위치 변수
 
         for(int i = 1; i<numbers.length; i++) {
 
             if(min > numbers[i]) {
                 min = numbers[i];
-                minPos=+i;
+                minPos=i+1;
             }
 
             if(max < numbers[i]) {
                 max = numbers[i];
-                maxPos=+1;
+                maxPos=i+1;
             }
         }
 
