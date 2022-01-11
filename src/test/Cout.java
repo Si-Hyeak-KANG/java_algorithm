cd6c146335a86ce3ca8d300c39a74cb6dbd98e24
Si-Hyeak-Kang <zlcls456@gmail.com>
2022-01-11 17:57:34 +0900

    이중 for문, ++i,i++ 활용 복습

---
+++ src/test/Cout.java
@@ -0,0 +1,31 @@
+package test;
+
+public class Cout {
+
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        System.out.println("i=" + i);
+
+        System.out.println("i++ = " + (i++));
+
+        System.out.println("i =" + i);
+
+        System.out.println("++i = " + (++i));
+
+        System.out.println("i = " + i);
+
+//        <이중 for문>
+
+//        for(i=1;i<=10;i++) {
+//
+//            for(j=1;j<=i;j++) {
+//
+//                System.out.print(j + ", ");
+//
+//            }
+//            System.out.println();
+//        }
+    }
+}
