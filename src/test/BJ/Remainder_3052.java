a999f6bf4e44dd669ab78919032ae8d2bfdbb5d5
Si-Hyeak-Kang <zlcls456@gmail.com>
2022-01-30 16:48:46 +0900

    3052 풀이 첫번째 방식

--- src/test/BJ/Remainder_3052.java
+++ src/test/BJ/Remainder_3052.java
@@ -2,19 +2,36 @@ package test.BJ;
 
 import java.util.Scanner;
 
+//서로 다른 나머지의 개수 구하기
 public class Remainder_3052 {
     public static void main(String[] args) {
 
+        // Scanner, 정규식 풀이 방법
         Scanner sc = new Scanner(System.in);
-        int[] num = new int[10];
-        int count = 0;
+        int[] arr = new int[10];
+        boolean bl; // 나머지가 동일한지 판단
+        int count = 0; //서로 다른 나머지 갯수
 
-        for(int i=1;i<=num.length;i++){
-            num[i-1]=sc.nextInt() % 42;
-            if(num[i-1]!=num[i]){
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt() % 42;
+        }
+
+        for(int i=0;i<arr.length;i++) {
+            bl = false; // 기본값 false
+
+            for(int j=i+1;j<arr.length;j++) {
+                if(arr[j]==arr[i]) {
+                    bl = true; // 두 수를 비교했을 때 동일할 시 no count
+                    break;
+                }
+            }
+            if(bl==false) {
                 count++;
             }
         }
+
         System.out.println(count);
+        // BufferedReader 이용 풀이
+        // BufferedReader, HashSet 이용 풀이 방법
     }
 }
