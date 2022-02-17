aadda612b59389aae12560c39a2f15f7ec320877
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-02-17 17:49:12 +0900

    팩토리얼_10872 문제 풀이

---
+++ src/test/BJ/Factorial_10872.java
@@ -0,0 +1,25 @@
+package test.BJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Factorial_10872 {
+
+    static int f(int N) {
+
+        if(N==0)
+            return 1;
+        else
+            return N * f(N-1);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int result = f(N);
+        System.out.println(result);
+    }
+}
