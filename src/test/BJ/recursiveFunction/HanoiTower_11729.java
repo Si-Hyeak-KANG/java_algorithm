7e5d49498c3b016288b9d8e81afee07d176454b6
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-02-19 19:56:45 +0900

    no message

--- src/test/BJ/recursiveFunction/HanoiTower_11729.java
+++ src/test/BJ/recursiveFunction/HanoiTower_11729.java
@@ -3,6 +3,7 @@ package test.BJ.recursiveFunction;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 /*7
 1 3
@@ -15,12 +16,15 @@ import java.io.InputStreamReader;
 * */
 
 public class HanoiTower_11729 {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()); // 원판 개수 입력
+    static void move(int n, int start, int sub, int end) {
 
     }
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); // 원판 개수 입력
+
+        move(n, 1,2,3);
+    }
 
 }
