109c6cccddf7b746703c0e6b96313cb26940a031
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-02-20 19:08:02 +0900

    하노이탑 문제 풀이 정답

--- src/test/BJ/recursiveFunction/HanoiTower_11729.java
+++ src/test/BJ/recursiveFunction/HanoiTower_11729.java
@@ -16,15 +16,37 @@ import java.util.Scanner;
 * */
 
 public class HanoiTower_11729 {
-    static void move(int n, int start, int sub, int end) {
+    public static StringBuilder sb = new StringBuilder();
 
+    public static void Hanoi(int N, int start, int mid, int to) {
+        // 이동할 원반의 수가 1개라면?
+        if (N == 1) {
+            sb.append(start + " " + to + "\n");
+            return;
+        }
+
+        // A -> C로 옮긴다고 가정할 떄,
+        // STEP 1 : N-1개를 A에서 B로 이동 (= start 지점의 N-1개의 원판을 mid 지점으로 옮긴다.)
+        Hanoi(N - 1, start, to, mid);
+
+        // STEP 2 : 1개를 A에서 C로 이동 (= start 지점의 N번째 원판을 to지점으로 옮긴다.)
+        sb.append(start + " " + to + "\n");
+
+        // STEP 3 : N-1개를 B에서 C로 이동 (= mid 지점의 N-1개의 원판을 to 지점으로 옮긴다.)
+        Hanoi(N - 1, mid, start, to);
     }
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine()); // 원판 개수 입력
 
-        move(n, 1,2,3);
+        int N = Integer.parseInt(br.readLine());
+
+        sb.append((int) (Math.pow(2, N) - 1)).append('\n');
+
+        Hanoi(N, 1, 2, 3);
+        System.out.println(sb);
     }
 
 }
+
