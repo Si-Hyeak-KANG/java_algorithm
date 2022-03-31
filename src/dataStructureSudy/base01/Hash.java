5e1a0e5fb987c4ae774cf7fd0e5a1daef2f417de
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-03-31 18:52:42 +0900

    Hash 기본 구현

--- src/dataStructureSudy/base01/Hash.java
+++ src/dataStructureSudy/base01/Hash.java
@@ -1,104 +1,65 @@
 package dataStructureSudy.base01;
 
+// 기본적인 해시 테이블 구현
 public class Hash {
 
-    // hash table -> 배열로 할당이 되어있다.
+    // Hash table
     public Slot[] hashTable;
 
+    // Hash 객체를 생성할 때 table 사이즈 지정
     public Hash(Integer size) {
-        this.hashTable = new Slot[size]; // 미리 사이즈 할당
+        this.hashTable = new Slot[size];
     }
 
+    // Slot에는 value를 가짐
     public class Slot {
 
-        String key;
         String value;
-        Slot next;
 
-        Slot(String key, String value) {
-            this.key = key;
+        Slot(String value) {
             this.value = value;
-            this.next = null;
         }
     }
 
-    /*
-    String name = "DaveLee";
-    name.charAt(0); // 문자열에서 index에 해당하는 문자 출력
+    //Hash function
+    public int hashFunction(String key) {
+        return (int)(key.charAt(0)) % this.hashTable.length; // 나머지지
+   }
 
-    (int)(name.charAt(0)); // 정수로 변환하면 아스키 코드로 정수값 출력
+   public boolean saveData(String key, String value) {
 
-    (int)(name.charAt(0)) % 20; // division 기법 : 나머지 리턴
-     */
+        Integer address = this.hashFunction(key); // key는 해시 함수를 거쳐서 해시 값(해시, 해시 주소)을 반환 -> 여기선 배열의 index와 동일
 
-    // 해시 함수 구현
-    public Integer hashFunction(String key) {
+       if(this.hashTable[address] != null) { // 해당 주소에 이미 데이터가 있을 경우
+           this.hashTable[address].value = value;
+       } else {
+           this.hashTable[address] = new Slot(value);
+       }
+       return true;
+   }
 
-        return (int)(key.charAt(0)) % this.hashTable.length;
+   public String getData(String key) {
 
-    }
-
-    // 저장을 성공했는가
-    public boolean saveData(String key, String value) {
-
-        Integer address = this.hashFunction(key);
-
-        if(this.hashTable[address] != null) { // 해당 주소에 이미 데이터가 존재할 경우
-
-            Slot findSlot = this.hashTable[address]; // head에 해당
-            Slot prevSlot = this.hashTable[address];
-
-            while (findSlot != null) {
-                if(findSlot.key == key) {
-                    findSlot.value = value;
-                    return true;
-                } else {
-                    prevSlot = findSlot;
-                    findSlot = findSlot.next;
-                }
-            }
-
-            prevSlot.next = new Slot(key, value);
-
-        } else {
-            this.hashTable[address] = new Slot(key, value);
-        }
-        return true;
-    }
-
-    public String getData(String key) {
         Integer address = this.hashFunction(key);
 
         if(this.hashTable[address] != null) {
-            Slot findSlot = this.hashTable[address];
-            while(findSlot != null) {
-                if(findSlot.key == key) {
-                    return findSlot.value;
-                } else {
-                    findSlot = findSlot.next;
-                }
-            }
-            return null;
-
+            return this.hashTable[address].value;
         } else {
             return null;
         }
-    }
+   }
 
     public static void main(String[] args) {
 
         Hash myHash = new Hash(20);
 
-        myHash.saveData("DaveLee","01094064180");
-        myHash.saveData("Leesoo","01098251090");
-        myHash.saveData("James","01044444180");
-        myHash.saveData("Daves","01012347180");
-        myHash.saveData("Dalyes","01099999999");
+        myHash.saveData("Lee","01098251090");
+        myHash.saveData("James","01094064180");
+        myHash.saveData("Denny","01013205230");
 
-        System.out.println(myHash.getData("LeeSoo"));
+        System.out.println(myHash.getData("Lee"));
         System.out.println(myHash.getData("James"));
-        System.out.println(myHash.getData("DaveLee"));
-        System.out.println(myHash.getData("Daves"));
-        System.out.println(myHash.getData("Dalyes"));
+        System.out.println(myHash.getData("Denny"));
+
     }
 }
