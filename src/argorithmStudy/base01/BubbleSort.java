88971bf05f2f0c9012b8d66833d3cd330033c483
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-04-09 23:45:48 +0900

    버블 정렬 코드 구현

--- src/argorithmStudy/base01/BubbleSort.java
+++ src/argorithmStudy/base01/BubbleSort.java
@@ -1,6 +1,27 @@
 package argorithmStudy.base01;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class BubbleSort {
 
-    
+    public static void main(String[] args) {
+
+        ArrayList<Integer> dataList = new ArrayList<Integer>();
+
+        dataList.add(2);
+        dataList.add(1);
+        dataList.add(4);
+
+        for(int i=0; i<dataList.size()-1;i++) {
+
+            if(dataList.get(i) > dataList.get(i+1)) {
+                Collections.swap(dataList,i,i+1);
+            }
+
+        }
+
+        System.out.println(dataList);
+
+    }
 }
