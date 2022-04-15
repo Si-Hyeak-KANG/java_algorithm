0efff4daf14b6b01ca14a3d91a45cb3bd024e6e9
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-04-15 19:51:49 +0900

    삽입 정렬 알고리즘 코드 구현

---
+++ src/argorithmStudy/base01/InsertionSort.java
@@ -0,0 +1,39 @@
+package argorithmStudy.base01;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class InsertionSort {
+
+    public ArrayList<Integer> sort(ArrayList<Integer> dataList) {
+
+        for(int i = 1; i < dataList.size(); i++) {
+
+            for(int j = i-1; j > -1; j--) {
+
+                if(dataList.get(i) < dataList.get(j)) {
+                    Collections.swap(dataList, i , j);
+                    i = j;
+                }
+            }
+
+        }
+        return dataList;
+
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> dataList = new ArrayList<>();
+
+        for(int i = 1; i < 50; i++ ) {
+            dataList.add((int)(Math.random() * 100));
+        }
+
+        InsertionSort insSort = new InsertionSort();
+
+        System.out.println(insSort.sort(dataList));
+    }
+
+
+}
