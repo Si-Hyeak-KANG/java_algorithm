9d547487d748ff2693ff8d458f0bb0b21d7c46f5
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-04-20 15:41:11 +0900

    병합 정렬

--- src/argorithmStudy/sort2/MergeSort.java
+++ src/argorithmStudy/sort2/MergeSort.java
@@ -1,9 +1,11 @@
 package argorithmStudy.sort2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MergeSort {
 
+    /*
     // for 문을 이용하여 만든 split
     public void split(ArrayList<Integer> array) {
 
@@ -23,13 +25,15 @@ public class MergeSort {
         System.out.println(firstArray);
         System.out.println(secondArray);
     }
+    */
+
 
     //ArrayList의 subList 활용
-    public void split2(ArrayList<Integer> array) {
+    public ArrayList<Integer> splitFunc(ArrayList<Integer> array) {
 
         // 배열의 크기가 1 이하면 나눌 것이 없음
         if(array.size() <= 1) {
-            return; // return 으로 메서드 종료
+            return array;
         }
 
         int medium = array.size() / 2;
@@ -37,26 +41,32 @@ public class MergeSort {
         ArrayList<Integer> leftArr = new ArrayList<>(array.subList(0, medium)); // array 의 인덱스 0번부터 medium -1 까지를 배열로 만듦
         ArrayList<Integer> rightArr = new ArrayList<>(array.subList(medium, array.size())); // array 의 인덱스 medium 부터 array.size() -1 까지를 배열로 만듦
 
-        System.out.println(leftArr);
-        System.out.println(rightArr);
+        return mergeFunc(leftArr, rightArr);
+    }
 
+    /**
+     * merge 함수
+     * @return 정렬된 배열을 반환
+     */
+    public ArrayList<Integer> mergeFunc(ArrayList<Integer> leftArr, ArrayList<Integer> rightArr) {
+
+        ArrayList<Integer> mergedList = new ArrayList<>();
+
+        int leftPoint = 0;
+        int rightPoint = 0;
+
+        // CASE 1 : left / right 둘 다 있을 때
+
+        // CASE 2 : right 데이터가 없을 때
+
+        // CASE 3 : left 데이터가 없을 때
+
+        return null;
     }
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> array = new ArrayList<>();
-
-        array.add(2);
-        array.add(3);
-        array.add(5);
-        array.add(6);
-        array.add(1);
-        array.add(4);
-        array.add(7);
-
         MergeSort test = new MergeSort();
-
-        test.split2(array);
-
+        
     }
 }
