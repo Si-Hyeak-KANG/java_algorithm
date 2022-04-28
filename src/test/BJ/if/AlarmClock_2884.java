fa1731b5ce797216020505907cc1c85c762fc10a
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-04-28 16:17:14 +0900

    백준_조건문_2884_알람시계_설명추가

--- src/test/BJ/if/AlarmClock_2884.java
+++ src/test/BJ/if/AlarmClock_2884.java
@@ -18,7 +18,10 @@ public class AlarmClock_2884 {
         int returnH = H; // 출력할 시간
         int returnM = M; // 출력할 분
 
+        // 일어나야할 분에서 45분을 뺐을 때 음수일 경우
         if (M < 0) {
+
+            // 시간은 0에서 -1을 빼면 23시가 되기 때문에 예외 처리를 해준다.
             if(H==0){
                 H = 24;
             }
