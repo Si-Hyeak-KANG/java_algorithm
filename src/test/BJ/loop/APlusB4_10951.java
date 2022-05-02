9c2e3b59b4a4dda91bd22a25b809988526c57fba
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-05-02 16:35:46 +0900

    백준_반복문_10951_A+B4

--- src/test/BJ/loop/APlusB4_10951.java
+++ src/test/BJ/loop/APlusB4_10951.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class APlusB_10951 {
+public class APlusB4_10951 {
 
     public static void main(String[] args) throws IOException {
 
