39055699c8e95d47520b0bcc25b3028d86223420
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-05-04 10:01:35 +0900

    백준 세개의 주사위 문제

---
+++ src/test/BJ/if/ThreeDice.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ThreeDice {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        if(A == B && B == C) {
+
+            
+        }
+
+        int sameNum = 1;
+    }
+}
