1018dbeaced9a44f53cdceed4e51db94c57f6e8b
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-05-11 00:15:16 +0900

    [백준_OX_Quiz_8598] rm import

--- src/test/BJ/array/OX_quiz_8598.java
+++ src/test/BJ/array/OX_quiz_8598.java
@@ -3,7 +3,6 @@ package test.BJ.array;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class OX_quiz_8598 {
