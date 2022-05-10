58793e4abb1b9b84aeda7c65bf12434082fd12be
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-05-10 23:52:04 +0900

    백준_4673_셀프넘버 풀이

---
+++ src/test/BJ/function/SelfNum_4673_2.java
@@ -0,0 +1,27 @@
+package test.BJ.function;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelfNum_4673_2 {
+
+    //10,000 보다 작
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+    // d(75) = 75 + 7 + 5 = 87;
+    // d(n)을 n과 n의 각 자리수를 더하는 함수
+    // 양의 정수 n, n, d(n), d(d(n)), d(d(d(n)))...
+    // n 은 d(n)의 생성자
+    // 생성자가 없는 숫자를 셀프 넘버
+
+    // 100 이하 셀프넘버 , 1, 3, 5, 7, 9, 20, 31, 42 ,53,64, 86, 97
+
+    // 91 + 9 + 1 == 101, 100 + 1 + 0 + 0 == 101;
+
+
+}
