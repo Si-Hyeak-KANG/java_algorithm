c2dad4e7cef36a822319897487e6ac12552f79d3
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-05-16 23:22:49 +0900

    클래스 이름 변경

--- src/test/BJ/math/SugarDelivery_2839.java
+++ src/test/BJ/math/SugarDelivery_2839.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class sugarDelivery_2839 {
+public class SugarDelivery_2839 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
