c1bfd8eceb00c2bf6e23f62f539dc20133e08cb5
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-05-18 01:01:40 +0900

    백준_달팽이는 올라가고 싶다_2869

--- src/test/BJ/math/UpSnail_2869.java
+++ src/test/BJ/math/UpSnail_2869.java
@@ -10,14 +10,19 @@ public class UpSnail_2869 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int A = Integer.parseInt(st.nextToken());
         int B = Integer.parseInt(st.nextToken());
         int V = Integer.parseInt(st.nextToken());
 
-        System.out.println(V % (A - B));
+        int result = (V - B) / (A - B);
 
+        if((V - B) % (A - B) != 0) {
+            result++;
+        }
+
+        System.out.println(result);
 
     }
 }
