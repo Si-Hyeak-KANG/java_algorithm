822633c42271e7b8a57bbc4d474b79b938dafe10
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-05-23 16:19:34 +0900

    백준_골드바흐의 추측_9020 - 주석추가

--- src/test/BJ/math/Goldbach_9020.java
+++ src/test/BJ/math/Goldbach_9020.java
@@ -15,6 +15,7 @@ import static java.lang.Math.sqrt;
  */
 public class Goldbach_9020 {
 
+    // 소수인지 판단하는 메서드
     static boolean isDecimal(int data) {
 
         boolean isTrue = true;
@@ -40,18 +41,24 @@ public class Goldbach_9020 {
         for(int i = 0; i < test; i++) {
 
             int n = Integer.parseInt(br.readLine()); // 짝수 n
-            int left = 0;
-            int right = 0;
+            int left = 0; // 파티션의 왼쪽 피연산자
+            int right = 0; // 파티션의 오른쪽 피연산자
 
+            // 골드바흐 수는 2+2, 3+3, 5+5, 7+7 과 같은 수를 제외하고는 모두 N/2 기점으로 왼쪽 소수와 오른쪽 소수의 합으로 이루어진다.
+            // 즉 n/2 이하만 검색하면 됨
             for(int j = 2; j <= n/2; j++) {
 
+                // j가 소수가 아니라면, 패스!
                 if(isDecimal(j)) {
 
+                    // n - j 값이 소수면, 파티션의 왼쪽과 오른쪽에 값 할당
                     if(isDecimal(n - j)) {
                         left = j;
                         right = n -j;
                     }
                 }
+
+                // 수의 크기가 커질 수록 파티션 왼쪽과 오른쪽의 차이가 적어짐.
             }
 
             System.out.println(left + " " + right);
