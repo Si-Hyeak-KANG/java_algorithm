c35ea4508298cf1714d048f59276f833c57d806d
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-06-05 23:09:56 +0900

    백준 한수 문제 풀이

--- src/test/BJ/function/Hansu_1065.java
+++ src/test/BJ/function/Hansu_1065.java
@@ -6,16 +6,47 @@ import java.io.InputStreamReader;
 
 public class Hansu_1065 {
 
-    static void func(int num) {
+    static boolean func(int num) {
 
+        if(num == 1000) {
+            return false;
+        }
 
+        if(num < 100) {
+            return true;
+        }
+
+        int first = num % 10;
+        num /= 10;
+        int second = num  % 10;
+        num /= 10;
+        int third = num % 10;
+
+        if(third - second == second - first) {
+            return true;
+        }
+
+        return false;
     }
 
     public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int N = Integer.parseInt(br.readLine());
 
+        boolean[] arr = new boolean[N];
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = func(i+1);
+        }
+
+        for (boolean b : arr) {
+            if(b == true) count++;
+        }
+
+        System.out.println(count);
     }
-
 
 }
