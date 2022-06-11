879d48638ac612041e4bd3e039af50b5bed928b5
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-06-11 17:40:54 +0900

    스택 수열 문제 주석 추가

--- src/test/BJ/stack/StackSequence_1874.java
+++ src/test/BJ/stack/StackSequence_1874.java
@@ -8,7 +8,7 @@ import java.util.Stack;
 /**
  * 1~N 까지의 스택 생성
  * push와 입력한 수열처럼
- *
+ * <p>
  * 수행시간 332ms
  */
 public class StackSequence_1874 {
@@ -18,23 +18,30 @@ public class StackSequence_1874 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
-        Stack<Integer> currStack = new Stack<>();
+        int N = Integer.parseInt(br.readLine()); // 정수 N 입력
+        Stack<Integer> currStack = new Stack<>(); // 1 부터 N 까지 오름차순으로 쌓을 스택
 
-        int top = 0;
+        int top = 0; // 스택의 가장 위에 있는 값값
 
         while (N > 0) {
 
+            // 수열 입력
             int input = Integer.parseInt(br.readLine());
 
+            // 입력한 값이 top 보다 크다면,
             if (input > top) {
 
+                // 스택에 Push 하고, sb에 + 를 append
                 for (int i = top + 1; i <= input; i++) {
                     currStack.push(i);
                     sb.append('+').append('\n');
                 }
+                // 입력한 값이 스택의 가장 위에 위치한 값이 되므로, top에 대입
                 top = input;
-            } else if(currStack.peek() != input) {
+
+            // 반례 : 스택에서 가장 위에 있는 값이 input과 같지 않다면,
+            } else if (currStack.peek() != input) {
+                // 결과로 No 를 출력하고 프로그램 종료
                 System.out.println("NO");
                 return;
             }
--- src/test/BJ/stack/StackSequence_1874_2.java
+++ src/test/BJ/stack/StackSequence_1874_2.java
@@ -17,16 +17,18 @@ public class StackSequence_1874_2 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine()); // 정수 N 입력
         Stack<Integer> stack = new Stack<>();
         int[] sequenceArr = new int[N];
 
+        // 입력 받는 수열을 배열에 넣음
         for(int i = 0; i < N; i++) {
-
             sequenceArr[i] = Integer.parseInt(br.readLine());
         }
 
+        // 스택에 쌓을 값
         int value = 1;
+        // 배열 인덱스
         int i = 0;
 
         while(value<=N) {
@@ -34,15 +36,21 @@ public class StackSequence_1874_2 {
             stack.push(value);
             sb.append("+").append("\n");
 
+            // 배열의 해당 인덱스 값이 스택에 있으면,
             while(stack.contains(sequenceArr[i])) {
+
+                // 해당 인덱스의 값이 스택의 가장 위에 있는 값이면,
                 if (stack.peek() == sequenceArr[i]) {
                     stack.pop();
                     sb.append("-").append("\n");
                     i++;
 
+                    // IndexOutOfBounds Exception 방지
                     if(i > N-1) {
                         break;
                     }
+
+                // 반례 해당 인덱스의 값이 스택의 가장 위에 있는 값이 아니라면,
                 } else {
                     System.out.println("NO");
                     return;
@@ -52,9 +60,7 @@ public class StackSequence_1874_2 {
             value++;
         }
 
-
         System.out.println(sb);
-
 
     }
 }
