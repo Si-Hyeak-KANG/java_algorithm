485abb9173faf23d6db74bf728a204a20fe11f1c
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-06-19 17:09:49 +0900

    백준 백트래킹 문제풀이

--- src/test/BJ/backTracking/NAndM1_15649.java
+++ src/test/BJ/backTracking/NAndM1_15649.java
@@ -3,8 +3,10 @@ package test.BJ.backTracking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+// 자기 자신을 제외한 번호와 연결된 수 나열
 public class NAndM1_15649 {
 
     public static void main(String[] args) throws IOException {
@@ -12,15 +14,48 @@ public class NAndM1_15649 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken()); // row
+        int M = Integer.parseInt(st.nextToken()); // column
 
-        int[][] arr = new int[N][M];
+        func(N,M,0,new ArrayList<>());
 
-        for(int i = 0; i < N; i++) {
+    }
 
-            arr[i][0] = i+1;
+    public static void func(int N, int M, int currColumn, ArrayList<Integer> currCandidate) {
+        StringBuilder sb = new StringBuilder();
+
+        if (currColumn == M) {
+
+            for (Integer data : currCandidate) {
+                sb.append(data+1).append(" ");
+            }
+
+            System.out.println(sb);
+            return;
         }
 
+        for (int i = 0; i < N; i++) {
+
+            if(isAvailable(currCandidate,i)) {
+
+                currCandidate.add(i);
+                func(N,M,currColumn+1,currCandidate);
+                currCandidate.remove(currCandidate.size()-1);
+            }
+
+        }
+    }
+
+    public static boolean isAvailable(ArrayList<Integer> currCandidate, int row) {
+
+        int currColumn = currCandidate.size();
+
+        // currCandidate 가 갖고 있는 값과 겹치면 false
+        for(int i = 0; i < currColumn; i++) {
+            if (currCandidate.get(i) == row) {
+                return false;
+            }
+        }
+        return true;
     }
 }
