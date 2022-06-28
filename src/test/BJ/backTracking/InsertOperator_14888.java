32da6c03c9c375833712b8ca452426fa59b81bf8
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-06-28 23:35:15 +0900

    no message

--- src/test/BJ/backTracking/InsertOperator_14888.java
+++ src/test/BJ/backTracking/InsertOperator_14888.java
@@ -3,7 +3,6 @@ package test.BJ.backTracking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class InsertOperator_14888 {
@@ -12,7 +11,40 @@ public class InsertOperator_14888 {
     static int minResult = Integer.MAX_VALUE;
     static int[] numberArr;
     static int[] operator = new int[4];
-    static int total = 0;
+
+    public static void func(int num, int index) {
+
+        if (index == numberArr.length) {
+            maxResult = Math.max(maxResult, num);
+            minResult = Math.min(minResult, num);
+            return;
+        }
+
+        for (int i = 0; i < operator.length; i++) {
+
+            if (operator[i] > 0) {
+
+                operator[i]--;
+
+                switch (i) {
+                    case 0:
+                        func(num + numberArr[index], index + 1);
+                        break;
+                    case 1:
+                        func(num - numberArr[index], index + 1);
+                        break;
+                    case 2:
+                        func(num * numberArr[index], index + 1);
+                        break;
+                    case 3:
+                        func(num / numberArr[index], index + 1);
+                        break;
+                }
+
+                operator[i]++;
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,32 +54,18 @@ public class InsertOperator_14888 {
         numberArr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             numberArr[i] = Integer.parseInt(st.nextToken());
         }
-
-        for(int i = 0; i < 4; i++) {
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 4; i++) {
             operator[i] = Integer.parseInt(st.nextToken());
         }
 
         // 메서드 실행
-        func(numberArr[0],0);
+        func(numberArr[0], 1);
 
+        System.out.println(maxResult);
+        System.out.println(minResult);
     }
-
-    public static void func(int num, int index) {
-
-
-
-
-    }
-
-    public static boolean isAvailable() {
-
-        return true;
-    }
-
-
-
-
 }
