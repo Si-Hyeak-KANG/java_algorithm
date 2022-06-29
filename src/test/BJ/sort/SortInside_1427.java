4a218c2e8f9e136245bc4c4d935708926de00668
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-06-29 19:44:23 +0900

    no message

--- src/test/BJ/sort/SortInside_1427.java
+++ src/test/BJ/sort/SortInside_1427.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 // 수행시간 124ms
-public class SortInside {
+public class SortInside_1427 {
 
     public static void main(String[] args) throws IOException {
 
