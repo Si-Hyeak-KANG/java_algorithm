a5af23569fc6ed3c69c4b53b44430f6062bd8fda
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-07-05 17:38:30 +0900

    no message

--- src/test/BJ/priorityqueue/MaxHeap_11279.java
+++ src/test/BJ/priorityqueue/MaxHeap_11279.java
@@ -4,38 +4,90 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 
 // 소름끼치게도 내부구조가 Heap 으로 이루어진 PriorityQueue 클래스 있음,,,,
 public class MaxHeap_11279 {
 
+    // Heap 처럼 사용할 배열을 멤버변수로 선언
     static ArrayList<Integer> myHeap = new ArrayList<>();
 
-    static Integer poll(){
+    /**
+     * 입력값이 0일 경우 값을 꺼내는 메서드
+     * @return
+     */
+    static Integer poll() {
 
-        Integer currRoot = myHeap.get(1);
+        Integer currRootValue = myHeap.get(1); // 출력할 루트
+
         int parentIndex = 1;
-        int leftChildIndex = parentIndex * 2;
-        int rightChildIndex = parentIndex * 2 + 1;
 
-        while() {
+        while (true) {
+
+            int leftChildIndex = parentIndex * 2;
+            int rightChildIndex = parentIndex * 2 + 1;
+
+            int changeChildIndex = 0; // 자리를 바꿀 자식 인덱스
+
+            // 자식 노드 둘다 없을 때
+            if(leftChildIndex >= myHeap.size()) {
+
+                myHeap.remove(parentIndex);
+                break;
 
             // 왼쪽 자식 노드와 오른쪽 자식 노드 크기 비교
             // case 1 : 왼쪽 자식 노드만 있을 때
-            if(rightChildIndex >= myHeap.size()) {
+            } else if (rightChildIndex >= myHeap.size()) {
 
-            }
+                changeChildIndex = leftChildIndex;
+
             // case 2 : 둘 다 있을 때
-            // 2-1 : 왼쪽이 클 때
-            // 2-2 : 오른쪽이 클 때
+            } else {
 
+                // 2-1 : 왼쪽이 클 때
+                if(myHeap.get(leftChildIndex) > myHeap.get(rightChildIndex)) {
+                    changeChildIndex = leftChildIndex;
+                }
+                // 2-2 : 오른쪽이 클 때
+                else {
+                    changeChildIndex = rightChildIndex;
+                }
+            }
+
+            Collections.swap(myHeap, parentIndex, changeChildIndex);
+            parentIndex = changeChildIndex;
         }
 
-    }
-
-    static void insert() {
+        return currRootValue;
 
     }
 
+    /**
+     * 입력값이 1이상일 경우 Heap 에 데이터 추가 메서드
+     * @param newData
+     */
+    static void insert(int newData) {
+
+        // 배열의 제일 뒤에 데이터 추가
+        myHeap.add(newData);
+
+        // 새로 추가한 부분이 root 가 아닐 경우, 만약 기존의 배열에 데이터가 없었다면 새로운 데이터는 root 가 된다.(정렬X)
+        if (myHeap.size() > 2) {
+            int indexOfNewData = myHeap.size() - 1; // 새로 추가한 데이터의 인덱스
+
+            while (indexOfNewData > 1) {
+                // 새로 추가한 데이터가 부모 데이터보다 작은 경우 정렬 X
+                if (myHeap.get(indexOfNewData) < myHeap.get(indexOfNewData / 2)) {
+                    break;
+                }
+                // 새로 추가한 데이터가 부모 데이터보다 큰 경우 자리교체
+                Collections.swap(myHeap, indexOfNewData, indexOfNewData / 2);
+                indexOfNewData = indexOfNewData / 2; // 자리를 교체한 상태에서 반복 진행
+            }
+        }
+    }
+
+    // 테스트 실행행
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -45,50 +97,24 @@ public class MaxHeap_11279 {
         // 힙의 1/2 인덱스 특성을 활용하기 위해 배열의 0번째 인덱스에는 값을 0을 집어넣는다.
         myHeap.add(0);
 
-        for(int i = 0 ; i < N ; i++) {
+        for (int i = 0; i < N; i++) {
 
             int input = Integer.parseInt(br.readLine());
-            // 입력값이 0일 경우
-            if(input == 0) {
 
-                // case1 : 배열에 값이 없을 경우
-                if(myHeap.size() == 1 ) {
-                    sb.append(0).append('\n');
+            // CASE 1: input 이 0일 경우
+            if (input == 0) {
+                // 1-1) 배열에 저장된 데이터가 없을 경우
+                if (myHeap.size() == 1) {
+                    sb.append("0").append("\n");
                 }
-                // case2 : 배열에 값이 있을 경우
+                // 1-2) 배열에 저장된 데이터가 있을 경우
                 else {
-                    // index 1번이 최대값인 root node 이기 때문에 1번 인덱스의 값을 출력
-                    sb.append(myHeap.remove(1)).append('\n');
+                    sb.append(poll()).append("\n");
                 }
-
             }
-            // 입력값이 1 이상 자연수일 경우 : 배열에 값을 저장
-            else if(input > 0) {
-
-                // case1 : root node 에 값이 없을 경우
-                if(myHeap.size() < 2) {
-                    myHeap.add(input);
-                }
-
-                // case 2 : root node 에 값이 있을 경우
-                else {
-                    myHeap.add(input); // 배열의 마지막 인덱스에 값을 할당
-                    int newNodeIndex = myHeap.size()-1; // 새로 추가한 노드의 인덱스
-
-                    // 배열 끝까지 노드들을 비교하며 위치 정렬
-                    while(newNodeIndex > 1) {
-                        int parentNodeIndex = newNodeIndex / 2; // 새로 추가한 노드의 부모 노드
-                        // 2-1 : input 이 부모노드보다 클 경우
-                        if(myHeap.get(parentNodeIndex) <= myHeap.get(newNodeIndex)) {
-
-                            newNodeIndex = parentNodeIndex;
-                        }
-                        // 2-2 : input 이 부모노드보다 작을 경우
-                        else {
-                            break; // 정렬할 필요가 없음
-                        }
-                    }
-                }
+            // CASE 2: input 이 1이상 자연수일 경우
+            else {
+                insert(input);
             }
         }
 
