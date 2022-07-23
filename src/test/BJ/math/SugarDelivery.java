7d12581f1c5b32dbc6d8c677a36045b73c0e8622
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-07-23 23:32:01 +0900

    no message

---
+++ src/test/BJ/math/SugarDelivery.java
@@ -0,0 +1,24 @@
+package test.BJ.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SugarDelivery {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        while(N>0) {
+
+            if(N % 5 == 0) {
+
+            } else if(N % 3 ==0) {
+
+            }
+        }
+
+    }
+}
--- src/test/BJ/queue/PrinterQueue_1966.java
+++ src/test/BJ/queue/PrinterQueue_1966.java
@@ -1,4 +1,38 @@
 package test.BJ.queue;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+//3 1 0 5 4 2 1 2 3 4 6 0 1 1 9 1 1 1 // 1 2 5
 public class PrinterQueue_1966 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+
+        int testCase = Integer.parseInt(br.readLine());
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for(int i = 0 ; i < testCase; i++) {
+
+            st = new StringTokenizer(br.readLine());
+            ArrayList<Integer> queue = new ArrayList<>();
+
+            int N = Integer.parseInt(st.nextToken()); // 문자의 개수
+            int M = Integer.parseInt(st.nextToken()); // 문자의 위치
+
+            for(int j = 0; j < N; j++) {
+                st = new StringTokenizer(br.readLine());
+                queue.add(Integer.parseInt(st.nextToken()));
+            }
+
+            arr.add(queue.get(M));
+        }
+
+        arr.stream().mapToInt(m->m).sorted().forEach(System.out::println);
+
+    }
 }
