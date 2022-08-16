71c738f0d0e8afc9d1b7479495c41babd391278f
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-08-16 19:50:40 +0900

    백준 색종이 만들기 풀이

--- src/test/BJ/divideconquer/MakingConfetti_2630.java
+++ src/test/BJ/divideconquer/MakingConfetti_2630.java
@@ -1,7 +1,75 @@
 package test.BJ.divideconquer;
 
-public class MakingConfetti_2630 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class MakingConfetti_2630 {
+    // 색상 카운트 할 변수 및 색종이(board)
+    public static int white = 0;
+    public static int blue = 0;
+    public static int[][] board;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        board = new int[N][N];
+
+        StringTokenizer st;
+
+        for (int row = 0; row < N; row++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int col = 0; col < N; col++) {
+                board[row][col] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        divide(0, 0, N);
+
+        System.out.println(white);
+        System.out.println(blue);
     }
+
+    // row : 행, col : 열
+    public static void divide(int row, int col, int size) {
+
+        if(colorCheck(row,col,size)) {
+            if(board[row][col] == 0) {
+                white++;
+            }
+            else {
+                blue++;
+            }
+            return;
+        }
+
+        int newSize = size/2;
+
+        divide(row, col, newSize); // 2사분면
+        divide(row, col + newSize, newSize); // 1사분면
+        divide(row+newSize,col,newSize); // 3사분면
+        divide(row+newSize, col+newSize, newSize); // 4사분면
+    }
+
+    public static boolean colorCheck(int row, int col, int size) {
+
+        int color = board[row][col]; // 1, 0
+
+        for (int i = row; i < row + size; i++) {
+            for (int j = col; j < col + size; j++) {
+
+                if(board[i][j] != color) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
 }
