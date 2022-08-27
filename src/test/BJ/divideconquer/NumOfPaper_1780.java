a031b25198b8f1bd9ec7da15d83836a38c51ca49
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-08-27 23:26:27 +0900

    백준) 중이의 개수_분할 정복

---
+++ src/test/BJ/divideconquer/NumOfPaper_1780.java
@@ -0,0 +1,8 @@
+package test.BJ.divideconquer;
+
+public class NumOfPaper_1780 {
+
+    public static void main(String[] args) {
+        
+    }
+}
--- src/test/BJ/sort/SortByAge_10814.java
+++ src/test/BJ/sort/SortByAge_10814.java
@@ -3,6 +3,6 @@ package test.BJ.sort;
 public class SortByAge_10814 {
 
     public static void main(String[] args) {
-        
+
     }
 }
